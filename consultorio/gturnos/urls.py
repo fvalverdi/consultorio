244c87abda6e772b5ffc7a2be726fa285da99cfd
Qno <facundovalverdi.fv@gmail.com>
2016-05-17 18:15:19 -0300

    Agrego modificaciones en forms. , plantilla base, admin, templates, etc

--- consultorio/gturnos/urls.py
+++ consultorio/gturnos/urls.py
@@ -22,4 +22,9 @@ urlpatterns = [
         url(r'^medico/(?P<pk>[0-9]+)/edit/$', views.medico_edit, name='medico_edit'),
         url(r'^medico/(?P<pk>[0-9]+)/$', views.medico_detail),
         url(r'^medico/all/$', views.medico_all, name='medico_all'),
+
+        url(r'^historia/new/$', views.historia_new, name='historia_new'),
+        url(r'^historia/(?P<pk>[0-9]+)/edit/$', views.historia_edit, name='historia_edit'),
+        url(r'^historia/(?P<pk>[0-9]+)/$', views.historia_detail),
+        url(r'^historia/all/$', views.historia_all, name='historia_all'),
     ]
